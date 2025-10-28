b988191be36c2e63860ec0509d6e5b78708cc3ad
Casey Waweru <casey.waweru@gmail.com>
2025-10-28 09:23:30 +0300

    Mastering fetch requests with useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,49 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [images, setImages] = useState();
+  const [isLoaded, setisLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect (() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await res.json()
+        setImages(data)
+      } catch (error) {
+        setisLoaded(true);
+        setError(error);
+      } finally {
+        setisLoaded(true);
+      }
+    }
+
+    fetchData();
+
+    /* fetch("https://dog.ceo/api/breeds/image/random")
+      .then(r => r.json())
+      .then(
+        data => {
+          setisLoaded(true);
+          setImages(data);
+          console.log(data);
+        },
+        (error) => {
+          setisLoaded(true);
+          setError(error);
+        }
+      )*/
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }else if (!isLoaded) {
+    return <p>"Loading..."</p>
+  }else {
+    return <img src={images.message} alt="A Random Dog" />
+  }
+
+}
+
+export default App;
